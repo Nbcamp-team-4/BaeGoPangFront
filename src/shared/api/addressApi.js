8ca0e851115e1ca4ae1c2faab92aeb65857f1930
Choi mieun <45choi22@gmail.com>
2026-03-11 01:54:02 +0900

    Feat: 사용자 홈 화면 가게 전체 조회 기능 추가

--- src/shared/api/addressApi.js
+++ src/shared/api/addressApi.js
@@ -6,3 +6,13 @@ export const getAddresses = async () => {
     method: 'GET'
   });
 };
+
+// 디폴트 주소 조회
+export const getDefaultAddress = async () => {
+  const addressRes = await getAddresses();
+  const addressJson = await addressRes.json();
+  const addressList = addressJson?.content ?? addressJson?.data?.content ?? [];
+
+  const defaultAddress = addressList.find((addr) => addr.isDefault === true);
+  return defaultAddress || null;
+};
